0026ba1ea146f3cd06189e78df79c6a6e65812ee
nepties <nepties@gmail.com>
2021-12-31 04:42:15 +0900

    draw line of box by loop

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+
+  renderSquare() {
+    return <Square />;
+  }
+
+  render() {
+    const size = 9;
+    let row = Array(size).fill(null);
+    for (let i = 0; i < size; i++) {
+      row[i] = this.renderSquare();
+    }
+
+    return (
+      <div>
+        <div className="board-row">
+            {row}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
