d7820ea498474c78e3336ea7562497203f8a119c
nepties <nepties@gmail.com>
2022-01-31 16:34:58 +0900

    add MUI style Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Main, Characters, Equipments, Sudoku } from "./pages";
+
 import "./App.css";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/characters" element={<Characters />} />
