36734556e6934b90e93bf272072f3d220ee86653
nepties <nepties@gmail.com>
2022-02-04 20:24:10 +0900

    add asset index file

---
+++ src/assets/index.tsx
@@ -0,0 +1,6 @@
+export { default as element_fire } from "./element_fire.webp";
+export { default as element_water } from "./element_water.webp";
+export { default as element_wind } from "./element_wind.webp";
+export { default as element_thunder } from "./element_thunder.webp";
+export { default as element_light } from "./element_light.webp";
+export { default as element_dark } from "./element_dark.webp";
