23c83270a0a2bee2500f99b1660e56b6d7d0e238
nepties <nepties@gmail.com>
2022-02-04 20:22:58 +0900

    move type files to type folder. add images.d.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,10 @@
 export type SudokuValue = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
 export type NullableSudokuValue = SudokuValue | null;
+
+export type ButtonInfo = {
+  value: string;
+  tooltip: string;
+  image: any;
+  text: string;
+};
